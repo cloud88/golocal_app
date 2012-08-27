100307fa18a7c51b4c076a91304da6dd99db52a9
Eugene Chu <eugenewchu@gmail.com>
2012-08-27 19:36:40 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolocalApp::Application.config.secret_token = 'bc9996d88569083a4d19f798ea6e5579bc43d2d71ec79c4764d282beba74e21cb0f116f853f8dab0f38d01bdce6604660b334f90dac3e47ec709df3e4bd59016'
